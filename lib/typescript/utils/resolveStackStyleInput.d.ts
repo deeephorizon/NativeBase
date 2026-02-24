169e83ae17c3a44cd6205521a27d3f85443a7cbd
Nelson Pinto <nelson.g.p@gmail.com>
2026-02-24 22:14:18 +0000

    - Generated /lib folder

---
+++ lib/typescript/utils/resolveStackStyleInput.d.ts
@@ -0,0 +1,7 @@
+export declare const resolveStackStyleInput: (variant: any, color: any) => {
+    outlineWidth: number;
+    boxShadow: string;
+} | {
+    outlineWidth: number;
+    boxShadow?: undefined;
+};
